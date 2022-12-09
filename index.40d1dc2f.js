9de36c7568c4ad06b8d3a0593ce7514d0887ad9b
Stanislav-She <116562198+Stanislav-She@users.noreply.github.com>
2022-12-09 09:32:50 +0000

    Deploying to gh-pages from @ Stanislav-She/food-delivery-project@5e46671fab46e92b19077d44bbfbb5be66834168 🚀

---
+++ index.40d1dc2f.js
@@ -0,0 +1,2 @@
+var btnUp={el:document.querySelector(".back-to-top"),show:function(){this.el.classList.remove("back-to-top__hide")},hide:function(){this.el.classList.add("back-to-top__hide")},addEventListener:function(){var t=this;window.addEventListener("scroll",(function(){(window.scrollY||document.documentElement.scrollTop)>400?t.show():t.hide()})),document.querySelector(".back-to-top").onclick=function(){window.scrollTo({top:0,left:0})}}};btnUp.addEventListener();
+//# sourceMappingURL=index.40d1dc2f.js.map
