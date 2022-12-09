9bfcf225be18fc208e544a8b6dbf08e6a33d8f2b
Stanislav-She <116562198+Stanislav-She@users.noreply.github.com>
2022-12-09 09:20:35 +0000

    Deploying to gh-pages from @ Stanislav-She/food-delivery-project@de65c58267f107f50b19ecad6c789b9b0e6cad27 🚀

---
+++ index.bba4ae29.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({prevArrow:'<button type="button" class="my-slick-prev"></button>',nextArrow:'<button type="button" class="my-slick-next"><svg class="slick__icon width="17" hight="29"./images/hero/symbol-defs.svg#icon-next"></use></svg></button>',infinite:!0,speed:300,slidesToShow:1,slidesToScroll:1,adaptiveHeight:!0});
+//# sourceMappingURL=index.bba4ae29.js.map
